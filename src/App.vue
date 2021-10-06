2d62ebd4bd0f4b8da188e7b7220e69368b287652
Princewill <seconsoft@gmail.com>
2021-10-06 20:23:06 +0100

    Mobile Navigation

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
         <div class="app-body">
             <TopBar />
             <router-view></router-view>
+
+            <MobileNavigation />
             <Footer copyright="©2021 EntainHub Nig. Ltd. All rights reserved" />
         </div>
     </div>
@@ -14,11 +16,12 @@
     import Header from "./components/public/Header.vue";
     import Footer from "./components/public/Footer.vue";
     import TopBar from "./components/public/TopBar.vue";
-    
+    import MobileNavigation from "./components/public/MobileNavigation.vue";
+
     export default {
         name: 'App',
         components: {
-            Header, Footer, TopBar
+            Header, Footer, TopBar, MobileNavigation
         }
     }
 </script>
---
+++ src/components/public/MobileNavigation.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="mobile-navigation">
+      <ul>
+        <li>
+            <router-link to="/music">
+                <center>
+                    <img src="static/svg/mobile-music.svg" class="mobile-nav-icon"> 
+                    <h4>Music</h4>
+                </center>
+            </router-link>
+        </li>
+        <li>
+            <router-link to="/video">
+                <center>
+                    <img src="static/svg/mobile-video.svg" class="mobile-nav-icon"> 
+                    <h4>Video</h4> 
+                </center>
+            </router-link>
+        </li>
+        <li>
+            <router-link to="/gist">
+                <center>
+                    <img src="static/svg/mobile-gist.svg" class="mobile-nav-icon" style="width:30px;"> 
+                    <h4>Gist</h4> 
+                </center>
+            </router-link>
+        </li>
+        <li>
+            <router-link to="/profile">
+                <center>
+                    <img src="static/svg/mobile-profile.svg" class="mobile-nav-icon"> 
+                    <h4>Sign Up</h4> 
+                </center>
+            </router-link>
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "MobileNavigation",
+        data(){
+            return {
+                is_mobile: false
+            }
+        },
+        created(){
+            this.is_mobile = window.is_mobile();
+        }
+    }
+</script>
+
+<style>
+    .mobile-navigation ul{
+        margin: 0px;
+        padding: 0px;
+        list-style: none;
+        color: white;
+    }
+    .mobile-navigation li{
+        float: left;
+        color: white;
+        width: 25%;
+    }
+    .mobile-navigation li h4{
+        font-size: 11px;
+        color: white;
+        padding: 0px;
+        margin: 0px;
+    }
+    .mobile-navigation{
+        width: 100%;
+        height: 80px;
+        position: fixed;
+        top: 88%;
+        left: 0%;
+        background-color: rgba(23, 23, 26, 1);
+        box-shadow: 0px -1px 5px rgba(250,250,250, 0.3);
+        padding: 10px 0px;
+        z-index: 2 !important;
+    }
+    .mobile-nav-icon{
+        width: 25px;
+    }
+</style>
